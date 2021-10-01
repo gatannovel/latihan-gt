aad283a278e5b2ef56fd78b9dff05446ab6bfb89
gatan nouval <gatannovel@gmail.com>
2021-10-01 16:00:40 +0700

    <menambahkan file pustaka-kujang>

---
+++ PUSTAKA-KUJANG/application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 estends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo"Nama saya gatan Nouval
+             saya tinggal di bogor hobi
+             game";
+    }
+
+}
