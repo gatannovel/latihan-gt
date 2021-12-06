084a119d90fca334ec56b146355aeddaf5c4c408
gatannovel <gatannovel@gmail.com>
2021-12-06 22:18:02 +0700

    pertemua 9

--- PUSTAKA-KUJANG/application/models/ModelUser.php
+++ PUSTAKA-KUJANG/application/models/ModelUser.php
@@ -1,30 +1,35 @@
-<?php
-    defined('BASEPATH') or exit('No direct script access allowed');
-    class ModelUser extends CI_Model{
-        public function simpanData($data = null){
-            $this->db->insert('user', $data);
-        }
+<?php 
+defined('BASEPATH') or exit('No direct script access allowed'); 
 
-        public function cekData($where = null){
-            return $this->db->get_where('user', $where);
-        }
+class ModelUser extends CI_Model 
+{ 
+    public function simpanData($data = null) 
+    { 
+        $this->db->insert('user', $data); 
+    } 
+    
+    public function cekData($where = null) 
+    { 
+        return $this->db->get_where('user', $where); 
+    } 
+    
+    public function getUserWhere($where = null) 
+    { 
+        return $this->db->get_where('user', $where); 
+    } 
 
-        public function getUserWhere($where = null){
-            return $this->db->get_where('user', $where);
-        }
- 
-       public function cekUserAccess($where = null){
-            $this->db->select('*');
-            $this->db->from('access_menu');
-            $this->db->where($where);
-            return $this->db->get();
-        }
-
-        public function getUserLimit()
-        {
-        $this->db->select('*');
-        $this->db->from('user');
-        $this->db->limit(10, 0); return
-        $this->db->get();
-        } 
+    public function cekUserAccess($where = null) 
+    { 
+        $this->db->select('*'); 
+        $this->db->from('access_menu'); 
+        $this->db->where($where); return 
+        $this->db->get(); 
     }
+    public function getUserLimit() 
+    { 
+        $this->db->select('*'); 
+        $this->db->from('user'); 
+        $this->db->limit(10, 0); return 
+        $this->db->get(); 
+    } 
+}
